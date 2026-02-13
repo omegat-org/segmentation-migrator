8eba1d699d15bbe259caeda85c7480244f9b5b5a
Hiroshi Miura <miurahr@linux.com>
2026-02-13 11:22:01 +0900

    chore: centralize Gradle version management using libs.versions.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ if (dotgit.exists()) {
 
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.BIN
-    gradleVersion = "9.3.0"
+    gradleVersion = libs.versions.gradle.get()
 }
 
 repositories {
